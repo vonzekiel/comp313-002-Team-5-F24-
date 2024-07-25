b87ba66c618c65e0d95f05ffc8a90cd292572bd3
Von Manaois <145621042+vonzekiel@users.noreply.github.com>
2024-07-25 14:03:27 -0400

    add account page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  const isLoggedIn = !!(currentUser && currentUser.username);
+  console.log(isLoggedIn);
   return (
     <header className="bg-black shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -32,8 +36,14 @@ function Header() {
           <FaSearch className="text-slate-600" />
         </form>
         <ul>
-          <Link to="/sign-in">
-            <li className="text-white font-bold hover:underline">Sign in</li>
+          <Link to="/profile">
+            {isLoggedIn ? (
+              <li className="text-white font-bold hover:underline">
+                My Account
+              </li>
+            ) : (
+              <li className="text-white font-bold hover:underline">Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
