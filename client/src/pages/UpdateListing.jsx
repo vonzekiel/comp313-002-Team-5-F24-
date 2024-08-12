198c13cb0d708614186487d01c495ea5ad8c60b7
Von Manaois <145621042+vonzekiel@users.noreply.github.com>
2024-08-11 22:42:14 -0400

    bug fix

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -1,177 +1,386 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import ListingItem from "../components/ListingItem";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 import { useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import Loading from "../components/Loading";
 
-export default function MyListing() {
+export default function UpdateListing() {
   const { currentUser } = useSelector((state) => state.user);
-  const [listings, setListings] = useState([]);
-  const [favorites, setFavorites] = useState(() => {
-    const savedFavorites = localStorage.getItem("favorites");
-    return savedFavorites ? JSON.parse(savedFavorites) : [];
+  const navigate = useNavigate();
+  const params = useParams();
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+    name: "",
+    description: "",
+    address: "",
+    type: "rent",
+    bedrooms: 1,
+    bathrooms: 1,
+    price: 50,
+    offer: false,
+    parking: false,
+    furnished: false,
   });
-  const [favoriteListings, setFavoriteListings] = useState([]);
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  // Fetch all user listings
   useEffect(() => {
-    const fetchListings = async () => {
-      try {
-        const res = await fetch(`/api/user/listings/${currentUser._id}`);
-        const data = await res.json();
-        if (data.success === false) {
-          console.error("Error fetching listings:", data.message);
-          return;
-        }
-        setListings(data);
-      } catch (error) {
-        console.error("Error fetching listings:", error);
-      }
-    };
-
-    fetchListings();
-  }, [currentUser]);
-
-  // Fetch favorite listings based on favorite IDs
-  useEffect(() => {
-    const fetchFavoriteListings = async () => {
-      try {
-        const fetchListingPromises = favorites.map(async (listingId) => {
-          try {
-            const res = await fetch(`/api/listing/get/${listingId}`);
-            const data = await res.json();
-            if (data.success === false) {
-              console.error(
-                `Error fetching listing ${listingId}:`,
-                data.message
-              );
-              return null;
-            }
-            return data;
-          } catch (error) {
-            console.error(`Error fetching listing ${listingId}:`, error);
-            return null;
-          }
-        });
-
-        const favoriteListingsData = await Promise.all(fetchListingPromises);
-        console.log("Favorite listings fetched:", favoriteListingsData);
-        setFavoriteListings(
-          favoriteListingsData.filter((listing) => listing !== null)
-        );
-      } catch (error) {
-        console.error("Error fetching favorite listings:", error);
-      }
-    };
-
-    fetchFavoriteListings();
-  }, [favorites]);
-
-  const handleListingDelete = async (listingId) => {
-    try {
-      const res = await fetch(`/api/listing/delete/${listingId}`, {
-        method: "DELETE",
-      });
+    const fetchListing = async () => {
+      const listingId = params.listingId;
+      const res = await fetch(`/api/listing/get/${listingId}`);
       const data = await res.json();
       if (data.success === false) {
-        console.error("Error deleting listing:", data.message);
+        console.log(data.message);
+
         return;
       }
-      setListings((prev) =>
-        prev.filter((listing) => listing._id !== listingId)
+      console.log(data);
+      setFormData(data);
+    };
+
+    fetchListing();
+  }, [params.listingId]);
+
+  const handleImageSubmit = (e) => {
+    e.preventDefault();
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);
+      setImageUploadError(null);
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(null);
+          setUploading(false);
+        })
+        .catch(() => {
+          setImageUploadError(
+            "Image upload failed. Please only upload images that are below 2MB."
+          );
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError("You can only upload up to 6 images per listing.");
+      setUploading(false);
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
       );
+    });
+  };
+
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
+
+  const handleChange = (e) => {
+    if (e.target.id === "sale" || e.target.id === "rent") {
+      setFormData({
+        ...formData,
+        type: e.target.id,
+      });
+    }
+
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.checked,
+      });
+    }
+
+    if (
+      e.target.type === "number" ||
+      e.target.type === "text" ||
+      e.target.type === "textarea"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.imageUrls.length < 1)
+        return setError("You must upload at least one image.");
+      setLoading(true);
+      setError(null);
+      const res = await fetch(`/api/listing/update/${params.listingId}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      } else {
+        navigate(`/listing/${data._id}`);
+      }
     } catch (error) {
-      console.error("Error deleting listing:", error);
+      setError(error.message);
+      setLoading(false);
     }
   };
 
   return (
-    <div className="bg-gray-900 min-h-screen py-10">
-      <h1 className="text-3xl font-bold text-center text-white mb-8">
-        My Listings
-      </h1>
-
-      <h2 className="text-2xl font-bold text-center text-white mb-6">
-        Favorites
-      </h2>
-      <div className="flex flex-wrap justify-center gap-6 mb-8">
-        {favoriteListings.length === 0 ? (
-          <p className="text-center text-white">
-            You don't have any favorite listings at the moment
-          </p>
-        ) : (
-          favoriteListings.map((listing) => (
-            <ListingItem
-              key={listing._id}
-              listing={listing}
-              onFavoriteToggle={() => {}}
-              isFavorite={true}
+    <main className="p-6 w-full mx-auto h-screen mb-auto bg-cover bg-center bg-gray-900">
+      <div className="p-6 bg-gray-900 rounded-lg shadow-lg max-w-4xl mx-auto">
+        <h1 className="text-3xl font-semibold text-white text-center mb-6">
+          Edit Listing
+        </h1>
+        <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-4">
+            <input
+              type="text"
+              placeholder="Name"
+              className="border p-3 rounded-lg shadow-md text-gray-900"
+              id="name"
+              maxLength={50}
+              min={4}
+              required
+              onChange={handleChange}
+              value={formData.name}
             />
-          ))
-        )}
-      </div>
-
-      <h2 className="text-2xl font-bold text-center text-white mb-6">
-        My Listings
-      </h2>
-      <div className="flex flex-wrap justify-center gap-6">
-        {listings.length === 0 ? (
-          <p className="text-center text-white">
-            You don't have any listings at the moment
-          </p>
-        ) : (
-          listings.map((listing) => (
-            <div
-              key={listing._id}
-              className="max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800"
-            >
-              <Link to={`/listing/${listing._id}`}>
-                <img
-                  src={listing.imageUrls[0]}
-                  alt={listing.name}
-                  className="object-cover w-full h-48"
+            <input
+              type="text"
+              placeholder="Description"
+              className="border p-3 rounded-lg shadow-md text-gray-900"
+              id="description"
+              required
+              onChange={handleChange}
+              value={formData.description}
+            />
+            <input
+              type="text"
+              placeholder="Address"
+              className="border p-3 rounded-lg shadow-md text-gray-900"
+              id="address"
+              required
+              onChange={handleChange}
+              value={formData.address}
+            />
+            <div className="flex gap-4 flex-wrap">
+              <div className="flex items-center gap-2">
+                <input
+                  type="radio"
+                  id="sale"
+                  name="type"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={formData.type === "sale"}
                 />
-              </Link>
-              <div className="px-4 py-2">
-                <h1 className="text-xl font-bold text-gray-800 uppercase dark:text-white">
-                  {listing.name}
-                </h1>
-                <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                  {listing.description}
-                </p>
-                <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                  {listing.address}
-                </p>
-                <p className="mt-1 text-lg font-semibold text-gray-800 dark:text-white">
-                  ${listing.price}
-                </p>
+                <label htmlFor="sale" className="text-white">
+                  Sell
+                </label>
               </div>
-              <div className="flex items-center justify-between px-4 py-2 bg-gray-900">
-                <div className="flex flex-col">
-                  <button
-                    onClick={() => handleListingDelete(listing._id)}
-                    className="text-red-700 uppercase text-xs"
-                  >
-                    Delete
-                  </button>
-                  <Link to={`/update-listing/${listing._id}`}>
-                    <button className="text-green-700 uppercase text-xs">
-                      Edit
-                    </button>
-                  </Link>
-                </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="radio"
+                  id="rent"
+                  name="type"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={formData.type === "rent"}
+                />
+                <label htmlFor="rent" className="text-white">
+                  Rent
+                </label>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  id="parking"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={formData.parking}
+                />
+                <label htmlFor="parking" className="text-white">
+                  With Parking
+                </label>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  id="furnished"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={formData.furnished}
+                />
+                <label htmlFor="furnished" className="text-white">
+                  Furnished
+                </label>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  id="offer"
+                  className="w-5"
+                  onChange={handleChange}
+                  checked={formData.offer}
+                />
+                <label htmlFor="offer" className="text-white">
+                  Offer
+                </label>
               </div>
             </div>
-          ))
-        )}
+            <div className="flex gap-4 flex-wrap">
+              <div className="flex items-center gap-2">
+                <input
+                  type="number"
+                  id="bedrooms"
+                  min={1}
+                  max={10}
+                  required
+                  className="p-3 border border-gray-300 rounded-lg shadow-md text-gray-900"
+                  onChange={handleChange}
+                  value={formData.bedrooms}
+                />
+                <label htmlFor="bedrooms" className="text-white">
+                  Bedrooms
+                </label>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="number"
+                  id="bathrooms"
+                  min={1}
+                  max={10}
+                  required
+                  className="p-3 border border-gray-300 rounded-lg shadow-md text-gray-900"
+                  onChange={handleChange}
+                  value={formData.bathrooms}
+                />
+                <label htmlFor="bathrooms" className="text-white">
+                  Bathrooms
+                </label>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="number"
+                  id="price"
+                  min={50}
+                  max={100000000}
+                  required
+                  className="p-3 border border-gray-300 rounded-lg shadow-md text-gray-900"
+                  onChange={handleChange}
+                  value={formData.price}
+                />
+                <label htmlFor="price" className="text-white">
+                  Price
+                </label>
+              </div>
+            </div>
+          </div>
+          <p className="text-gray-300">
+            <span className="text-xs">(Max 6 images)</span>
+          </p>
+          <div className="flex flex-wrap gap-4 mb-4">
+            {formData.imageUrls.length > 0 &&
+              formData.imageUrls.map((url, index) => (
+                <div
+                  key={index}
+                  className="relative w-32 h-32 bg-gray-200 rounded-lg overflow-hidden"
+                >
+                  <img
+                    src={url}
+                    alt={`Listing Image ${index + 1}`}
+                    className="object-cover w-full h-full"
+                  />
+                  <button
+                    type="button"
+                    className="absolute top-1 right-1 text-white bg-red-500 p-1 rounded-full"
+                    onClick={() => handleRemoveImage(index)}
+                  >
+                    &times;
+                  </button>
+                </div>
+              ))}
+          </div>
+          <label
+            htmlFor="images"
+            className="w-full px-4 py-2 tracking-wide text-black transition-colors duration-300 transform bg-white rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50 disabled:opacity-50"
+          >
+            Choose Images
+            <input
+              type="file"
+              id="images"
+              multiple
+              accept="image/*"
+              className="hidden"
+              onChange={(e) => setFiles(e.target.files)}
+            />
+          </label>
+          {imageUploadError && (
+            <p className="text-red-500">{imageUploadError}</p>
+          )}
+          <button
+            type="button"
+            onClick={handleImageSubmit}
+            className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50 disabled:opacity-50"
+          >
+            Upload Images
+          </button>
+          <button
+            type="submit"
+            className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-green-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-green-400 focus:ring focus:ring-green-300 focus:ring-opacity-50 disabled:opacity-50"
+          >
+            {loading ? <Loading /> : "Update Listing"}
+          </button>
+          {error && <p className="text-red-500">{error}</p>}
+        </form>
       </div>
-      <div className="flex justify-center mt-5">
-        <Link
-          to={"/create-listing"}
-          className="px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50 disabled:opacity-50"
-        >
-          Create Listing
-        </Link>
-      </div>
-    </div>
+    </main>
   );
 }
